69f407a9f10a58c8a1b20f8722a2ad8536e1b389
IrakliMigriauli <106801855+IrakliMigriauli@users.noreply.github.com>
2022-06-19 17:46:35 +0400

    Add files via upload

---
+++ src/Components/Services/Services.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import './Services.css'
+import HeartEmoji from "../../img/HeartEmoji.png";
+import Glasses from "../../img/glasses.png";
+import Humble from "../../img/humble.png";
+import Card from '../Card/Card';
+import Resume from './resume.pdf'
+
+const Services = () => {
+  return (
+    <div className="services">
+
+
+        {/*  Left side  */}
+        <div className="awesome">
+            <span>My Awesome</span>
+            <span>Services</span>
+            <spane>Lorem ispum is simpley dummy text of printing of printing Lorems
+                <br />
+                ispum is simple dummy text of printing
+            </spane>
+            <a href={Resume} download>
+                <button className="button s-button">Download CV</button>
+            </a>
+            <div className="blur s-blur1" style={{ background: "#ABF1FF94"}} ></div>
+        </div>
+
+        {/* Right side */}
+        <div className="cards">
+
+                        {/* 1st CARD */}
+            <div style={{left: '14rem'}}>
+                <Card
+                    emoji = {HeartEmoji}
+                    heading = {'Design'}
+                    detail = {"Figma, Sketch, Photoshop, Adobe, Adobe xd"}
+                />
+            </div>
+
+                        {/* 2nd CARD */}
+
+            <div style={{top: "12rem", left: "-4rem"}}>
+                <Card
+                    emoji = {Glasses}
+                    heading = {"Developer"}
+                    detail = {"Html, Css, JavaScript, React"}
+                />
+            </div>
+
+                        {/* 3rd CARD */}
+            
+            <div style={{top: "19rem", left: "12rem"}}>
+                <Card
+                    emoji = {Humble}
+                    heading = {"UI/UX"}
+                    detail = {"Lorem ispum dummy text are usually use in section"}
+                />
+            </div>
+            <div className="blur s-blur2" style={{ background: "var(--purple"}}></div>
+        </div>
+    </div>
+  )
+}
+
+export default Services
